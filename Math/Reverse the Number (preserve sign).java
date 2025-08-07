9c1692eea434d9c3f9cf94c5392e63c45b8c999d
Thamaraiselvan <s.thamaraiselvan2005@gmail.com>
2025-08-07 18:39:40 +0530

    Create Reverse the Number (preserve sign).java

---
+++ Math/Reverse the Number (preserve sign).java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int r = 0;
+        
+        if(n<0){
+            System.out.print("-");
+            n = n * -1;
+        }
+     
+        while(n != 0)
+        {
+            r = r * 10 + (n%10);
+            n /= 10;
+        }
+        System.out.print(r);
+        
+        
+    }
+}
