e0e2b5c0f145329d11dc5d3fbfdf34a587aada1f
Thamaraiselvan <s.thamaraiselvan2005@gmail.com>
2025-08-08 18:46:58 +0530

    Create Birthday Cake Candles.java

---
+++ Arrays/Birthday Cake Candles.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void found(int[] a,int key,int n ){
+       
+        boolean istrue = false;
+        int count = 0;
+        
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(a[i] == key)
+                count++;
+          
+        }
+        System.out.println(count);
+       
+            
+    }
+    
+     
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[] = new int[n];
+        int max = Integer.MIN_VALUE;
+        
+        
+        
+        for(int i = 0; i<n ; i ++)
+        {
+            a[i] = scan.nextInt();
+        }
+        
+        
+        
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(a[i] >= max)
+                max = a[i];
+            
+        }
+        
+        found(a,max,n);
+     
+    }
+}
