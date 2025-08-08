af516fca3ed294dab6194d7dbeebc437f49c47f7
Thamaraiselvan <s.thamaraiselvan2005@gmail.com>
2025-08-08 18:07:03 +0530

    Create Target Locked.java

---
+++ Arrays/Target Locked.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[] = new int[n];
+        
+        
+        for(int i = 0; i<n ; i ++)
+        {
+            a[i] = scan.nextInt();
+        }
+        
+        int key = scan.nextInt();
+        boolean istrue = false;
+        
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(a[i] == key)
+            {System.out.println(i);
+                istrue = true;
+             break;
+            }
+            
+        }
+        if(!istrue)
+            System.out.println("-1");
+            
+        
+    }
+}
