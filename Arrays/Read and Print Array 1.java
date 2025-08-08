d5a9a6b0ae99c569c8e62dd7858c26343287d985
Thamaraiselvan <s.thamaraiselvan2005@gmail.com>
2025-08-08 11:20:29 +0530

    Create Read and Print Array 1.java

---
+++ Arrays/Read and Print Array 1.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[] = new int[n];
+        
+        for(int i = 0; i<n ; i ++)
+        {
+            a[i] = scan.nextInt();
+        }
+        
+        for(int i = 0 ; i < n ; i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+}
+
+
+/*Input (stdin)
+
+5
+10 20 30 40 50
+Your Output (stdout)
+
+10 20 30 40 50 */
