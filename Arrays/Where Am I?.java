c75b6da1a2c686a872dd0418dae4247c050beb6f
Thamaraiselvan <s.thamaraiselvan2005@gmail.com>
2025-08-08 18:09:21 +0530

    Create Where Am I?.java

---
+++ Arrays/Where Am I?.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[] = new int[n];
+        
+        
+        for(int i = 0; i<n ; i ++)
+        {
+            a[i] = scan.nextInt();
+        }
+        
+        int key = scan.nextInt();
+        boolean istrue = false;
+        
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(a[i] == key)
+            {System.out.print(i+" ");
+                istrue = true;}
+            
+        }
+        if(!istrue)
+            System.out.println("-1");
+            
+        
+    }
+}
